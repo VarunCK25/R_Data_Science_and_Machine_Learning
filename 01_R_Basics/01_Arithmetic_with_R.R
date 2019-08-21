1c5e407eba6eab5470d7463a8b2216bb0726730a
varun <varun.ck@aline-consulting.com>
2019-08-22 00:37:57 +0530

    Arithmetic with R

---
+++ 01_R_Basics/01_Arithmetic_with_R.R
@@ -0,0 +1,20 @@
+# Addition
+print(1 + 2)
+
+# Subtraction
+print(5 - 3)
+
+# Multiplication
+print(5*6)
+
+# Division
+print(1/2)
+
+# Exponents (Power of)
+print(2^3)
+
+# Modulus
+print(5 %% 2)
+
+# Order of Operations
+print((100 * 2) + (50 / 2))
